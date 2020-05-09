e0e3eeaa05337130e26f9bf2f6688585f4099b9f
afstroe <63409056+afstroe@users.noreply.github.com>
2020-05-09 21:49:53 +0100

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,4 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-ReactDOM.render(<h1>Hello World!</h1>, document.getElementById("root"));
+const fName = "Glad";
+const lName = "Menumorut";
+const luckyNumber = "13";
+
+ReactDOM.render(
+  <>
+    <h1>Hello {fName + " " + lName}!</h1>
+    <p>Your lucky number is {luckyNumber}.</p>
+  </>,
+  document.getElementById("root")
+);
